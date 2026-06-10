252b3e4e435c6422bd9f092dc26168669ad30d46
murali0127 <smuralidharan845@gmail.com>
2026-06-10 11:50:54 +0530

    removed data.ts from gitignore

---
+++ data.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+
+export const CATEGORIES = ['All', 'Tech', 'Design', 'Learning', 'Travel & Lifestyle', 'Business', 'Finance', 'Health & Fitness', 'Cooking'];;
+export const ACCENT_COLORS = [
+      { name: 'iOS Blue', value: '#007AFF' },
+      { name: 'Emerging Emerald', value: '#34C759' },
+      { name: 'Classic Gold', value: '#FFD700' },
+      { name: 'Volcanic Amber', value: '#FF9500' },
+      { name: 'Studio Purple', value: '#AF52DE' }
+];
